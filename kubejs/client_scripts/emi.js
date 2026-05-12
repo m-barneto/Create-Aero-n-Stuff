3de1e4dcb848b39ffd4614876c07cb7a0ff5c9bc
m-barneto <4347791+m-barneto@users.noreply.github.com>
2026-05-12 15:43:49 -0400

    removed EMI++ as its bad

--- kubejs/client_scripts/emi.js
+++
@@ -1,7 +0,0 @@
-EmiPlusPlusEvents.registerGroups(event => {
-    // Create a group from a Tag
-    event.register("cans:all_logs", "#minecraft:logs")
-
-    // Create a group for a specific item
-    event.register("cans:command_blocks", "minecraft:command_block")
-})
